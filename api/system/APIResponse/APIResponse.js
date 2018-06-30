071fb227ba06bf91f0e8499869c0b06c663da131
Vladimir Volchkov <vladi@howtoclient.com>
2018-07-01 01:19:25 +0300

    moved helpers to their own file fixed minor bugs

--- api/system/APIResponse/APIResponse.js
+++ api/system/APIResponse/APIResponse.js
@@ -1,56 +1,5 @@
 const
-    forceArray = (mixed) => Array.isArray(mixed) ? mixed : [mixed],
-    handleFunctionCall = (target, key, assignValue) => {
-        if (Array.isArray(assignValue)) {
-            return assignValue.forEach(
-                applyValues => target[key].apply(target, forceArray(applyValues))
-            );
-        }
-        if (typeof assignValue === "object") {
-            return Object.keys(assignValue).forEach(
-                valueKey =>
-                    target[key].apply(target,
-                        Array.isArray(assignValue[valueKey])
-                            ? assignValue[valueKey]
-                            : [valueKey, assignValue[valueKey]]
-                    )
-            );
-        }
-        target[key](assignValue);
-    },
-    assignSettings = (target, settings) => {
-        if (typeof target !== 'object' || typeof settings !== 'object') {
-            return
-        }
-        Object.keys(settings).forEach(key => {
-            const
-                targetValue = target[key],
-                assignValue = settings[key];
-
-            if (typeof targetValue === 'function') {
-                return handleFunctionCall(target, key, assignValue);
-            }
-
-            if (Array.isArray(targetValue) && Array.isArray(assignValue)) {
-                return target[key].push.apply(target[key], assignValue);
-            }
-
-            if (typeof targetValue === 'object' && typeof assignValue === 'object') {
-                return assignSettings(targetValue, assignValue)
-            }
-
-            target[key] = assignValue;
-        })
-    },
-    applySets = (res, sets) => {
-        Object.keys(sets).forEach(
-            key => {
-                res.set(key, sets[key])
-            }
-        )
-    };
-
-const
+    {assignSettings, applySets} = require('./helpers'),
     APISuccess = require('./responses/APISuccess'),
     APIError = require('./responses/APIError'),
     APIValidationError = require('./responses/APIValidationError'),
@@ -76,7 +25,7 @@ class APIResponse {
 
     success(data) { //200-204
         const response = new APISuccess(data);
-        this.applySettings(response, 200).send();
+        this.applySettings(response, data ? 200 : 204).send();
     }
 
     invalid(error) { //400
