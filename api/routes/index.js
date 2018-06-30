144f21380b54c35b3727220eae7c5811a4b549ca
Vladimir Volchkov <vladi@howtoclient.com>
2018-06-30 19:29:03 +0300

    updated packages

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 const
     router = require('express').Router(),
     notFound = require("./404"),
-    testRoutes = require('test-routes');
+    testRoutes = require('./test-routes');
 
 router.use('/',testRoutes);
 router.use('/',notFound);
