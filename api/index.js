7d0296cd914d704c3c49afe7be96ecb6ad650603
Vladimir Volchkov <vladi@howtoclient.com>
2018-06-30 19:38:12 +0300

    configured application and added morgan

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 const
+    bodyParser = require('body-parser'),
+    helmet = require('helmet'),
+    morgan = require('morgan'),
+    cors = require('./system/cors'),
+    errors = require('./system/errors'),
+    routes = require('./routes'),
     api = require('express')();
+
+
+api.set('json replacer', (key, value) => /^_/.test(key) ? undefined : value);
+api.use(morgan('tiny', {skip: (req, res) => req.method === "OPTIONS"}));
+api.use(cors);
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({extended: true}));
+api.use(helmet());
+api.use('/',routes);
+api.use('/',errors);
 
 module.exports=api;
